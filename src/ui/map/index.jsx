fe8b56241e6fdc9d1aae706e33bef97ee81b1688
Phonixoop <33152793+Phonixoop@users.noreply.github.com>
2022-12-02 16:37:08 +0330

    map import error fixed

--- src/ui/map/index.jsx
+++ src/ui/map/index.jsx
@@ -1,6 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
-import { useEffect, useLayoutEffect, useState } from "react";
 export default function Map({
   location = { lat: 0, lon: 0 },
   onChange = () => {},
