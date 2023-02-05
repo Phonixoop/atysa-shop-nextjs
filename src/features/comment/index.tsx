9eaf2025ba1db788c37ceb37bffa64d0ea04da05
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-02-05 18:09:31 +0330

    time added to comment

--- src/features/comment/index.tsx
+++ src/features/comment/index.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import StarScore from "features/star-score";
+import moment from "jalali-moment";
 
 export default function Comment({ comment }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-5 w-full bg-atysa-primary  p-5 rounded-md">
-      <div className="w-full flex justify-start items-center gap-10 text-atysa-main text-sm">
-        <span> {comment.user_name}</span>
-        <div className="flex justify-start items-center flex-wrap gap-2">
-          {comment.products.map((product_name) => {
-            return (
-              <span
-                key={product_name}
-                className="bg-atysa-main text-white px-2 py-1 rounded-full text-xs"
-              >
-                {product_name}
-              </span>
-            );
-          })}
+    <div className="flex w-full flex-col items-center justify-center gap-5 rounded-md  bg-atysa-primary p-5">
+      <div className="flex w-full items-center justify-between gap-10 text-sm text-atysa-main">
+        <div className="flex  items-center justify-between gap-10 text-sm ">
+          <span className="font-bold text-atysa-main">{comment.user_name}</span>
+
+          <div className="flex flex-wrap items-center justify-start gap-2">
+            {comment.products.map((product_name) => {
+              return (
+                <span
+                  key={product_name}
+                  className="rounded-full bg-atysa-900 px-2 py-1 text-xs text-white"
+                >
+                  {product_name}
+                </span>
+              );
+            })}
+          </div>
         </div>
+        <span dir="rtl" className="text-xs font-bold text-atysa-900">
+          {moment(comment.created_at).locale("fa").format("d MMMM yyyy")}
+        </span>
       </div>
-      <div className="flex justify-between w-full">
+      <div className="flex w-full justify-between">
         <p>{comment.message}</p>
         <StarScore score={comment.rate_score} />
       </div>
