91413f8b0fc83e9783619cc7e439e7b11ec611a8
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-02-07 01:36:52 +0330

    console log removed

--- src/features/checkout/index.jsx
+++ src/features/checkout/index.jsx
@@ -46,7 +46,6 @@ export default function CheckoutView() {
           },
           coupon,
           onCouponResult: (coupon) => {
-            console.log({ coupon });
             if (coupon.isValid)
               setCoupon({
                 coupon_id: coupon.data.id,
